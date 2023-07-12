8bd359139392dda2837ca29279589b7a6b23a546
erraoudi-soufiane <erraoudisoufiane5@gmail.com>
2023-07-12 14:03:47 +0100

    homepage

---
+++ src/navigation/Sidenav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Sidenav.css"
+
+function Sidenav() {
+  return (
+    <div>
+      <img src="https://www.pngkey.com/png/full/828-8286178_mackeys-work-needs-no-elaborate-presentation-or-distracting.png" 
+      alt="instagram logo" 
+      className="sidenav__logo" />
+
+    <div className="sidenav__buttons">
+      <div className="sidenav__button">
+        <span>Home</span>
+      </div>
+    </div>
+    </div>
+
+  )
+}
+
+export default Sidenav
